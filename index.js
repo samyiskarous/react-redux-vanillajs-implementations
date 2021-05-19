b38a84beb354879deeff92c821aad0dd9b2ba8b5
Samy <samygeorge007@gmail.com>
2021-05-20 01:39:34 +0200

    feat: Working simple App of a Redux Store using Vanila JS and simple React behavior

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-import {createStore} from './redux-store'
+// Redux Store
+const createStore = (reducer) => {
+    // The store should have four parts
+    // 1. The state
+    // 2. Get the state.
+    // 3. Listen to changes on the state.
+    // 4. Update the state
+
+    let state
+    let listeners = []
+
+    const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
+    }
+
+    return {
+        getState,
+        subscribe,
+        dispatch,
+    }
+}
 
 // App Code
 const ADD_TODO = 'ADD_TODO'
@@ -60,14 +90,14 @@ function goals (state = [], action) {
     }
 }
 
-function app (state = {}, action) {
+function rootReducer (state = {}, action) {
     return {
         todos: todos(state.todos, action),
         goals: goals(state.goals, action),
     }
 }
 
-const store = createStore(app)
+const store = createStore(rootReducer)
 
 store.subscribe(() => {
     updateTodoAndGoalsLists();
